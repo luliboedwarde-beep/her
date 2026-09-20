ff8b0900ff3dcff5de2500ef9a93210d70a34c3d
luliboedwarde-beep <luliboedwarde@gmail.com>
2026-09-20 20:38:11 +0200

    Add files via upload

---
+++ heart letters/letter for HER/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['./letter.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        romantic: ['Cormorant Garamond', 'serif'],
+        sans: ['Inter', 'sans-serif']
+      },
+      colors: {
+        rose: {
+          50: '#fff7fa',
+          100: '#ffeef4',
+          200: '#ffd9e6',
+          300: '#ffb8ce',
+          400: '#f88eaf',
+          500: '#ed628d',
+          600: '#d94372',
+          700: '#b82e5c',
+          800: '#962849',
+          900: '#7d263f'
+        }
+      }
+    }
+  }
+};
